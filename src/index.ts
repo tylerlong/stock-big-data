1a5ebfe6cb308a0a2503ca214879e090bbac65e3
Tyler Long <tyler4long@gmail.com>
2021-02-23 16:28:53 -0800

    Refactor code

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,31 @@
 import * as puppeteer from 'puppeteer';
 import * as fs from 'fs';
 import waitFor from 'wait-for-async';
+import * as os from 'os';
+import * as path from 'path';
 
-(async () => {
-  const browser = await puppeteer.launch({headless: false});
-  const page = await browser.newPage();
-  await page.goto('https://finance.yahoo.com/quote/RNG/history');
-  const downloadButtonSelector = 'a[download="RNG.csv"]';
+const download = async (page: puppeteer.Page, symbol: string) => {
+  const downloadButtonSelector = `a[download="${symbol}.csv"]`;
+  const filePath = path.join(os.homedir(), 'Downloads', `${symbol}.csv`);
+  await page.goto(`https://finance.yahoo.com/quote/${symbol}/history`);
   await page.waitForSelector(downloadButtonSelector);
-  const filePath = '/Users/tyler.liu/Downloads/RNG.csv';
   if (fs.existsSync(filePath)) {
     fs.unlinkSync(filePath);
   }
   await page.click(downloadButtonSelector);
   await waitFor({
-    interval: 1000,
+    interval: 100,
     condition: () => {
       return fs.existsSync(filePath);
     },
   });
+  fs.renameSync(filePath, `./downloads/${symbol}.csv`);
+};
+
+(async () => {
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+  await download(page, 'RNG');
+  await download(page, 'ZM');
   await browser.close();
 })();
