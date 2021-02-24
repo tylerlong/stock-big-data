117e377bda1cd9831ebb43485b9a808b713ba3ab
Tyler Long <tyler4long@gmail.com>
2021-02-23 16:00:08 -0800

    Can download file

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
-const puppeteer = require('puppeteer');
+import * as puppeteer from 'puppeteer';
+import * as fs from 'fs';
+import waitFor from 'wait-for-async';
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
-  await page.goto('https://finance.yahoo.com/quote/RNG/history?p=RNG');
-  await page.screenshot({
-    path: 'RNG.png',
-    clip: {
-      x: 0,
-      y: 0,
-      width: 600,
-      height: 1200,
+  await page.goto('https://finance.yahoo.com/quote/RNG/history');
+  const downloadButtonSelector = 'a[download="RNG.csv"]';
+  await page.waitForSelector(downloadButtonSelector);
+  const filePath = '/Users/tyler.liu/Downloads/RNG.csv';
+  if (fs.existsSync(filePath)) {
+    fs.unlinkSync(filePath);
+  }
+  await page.click(downloadButtonSelector);
+  await waitFor({
+    interval: 1000,
+    condition: () => {
+      return fs.existsSync(filePath);
     },
   });
-
   await browser.close();
 })();
