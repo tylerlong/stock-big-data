afbee85afb80c7cc0243c5096d5da761e9235dc0
Tyler Long <tyler4long@gmail.com>
2021-02-24 21:52:24 -0800

    First working max command

--- src/max-change.ts
+++ src/max-change.ts
@@ -13,7 +13,36 @@ for (const file of files) {
   symbols[symbol] = {};
   for (const line of R.tail(text.split('\n'))) {
     const tokens = line.split(',');
-    symbols[symbol][tokens[0]] = R.tail(tokens).join(',');
+    symbols[symbol][tokens[0].replace(/-/g, '')] = {
+      open: tokens[1],
+      high: tokens[2],
+      low: tokens[3],
+      close: tokens[4],
+      aClose: tokens[5],
+      volume: tokens[6],
+    };
   }
 }
-console.log(symbols);
+// console.log(symbols);
+
+const startDate = '20210219';
+const endDate = '20210224';
+const list = [];
+
+for (const symbol of Object.keys(symbols)) {
+  console.log(symbol);
+  const start = symbols[symbol][startDate];
+  if (start === undefined) {
+    continue;
+  }
+  const end = symbols[symbol][endDate];
+  list.push({
+    symbol,
+    start,
+    end,
+    change: (end.close - start.close) / start.close,
+  });
+}
+
+const result = R.reverse(R.sortBy(R.pipe(R.prop('change'), Math.abs))(list));
+console.log(R.take(10, result));
