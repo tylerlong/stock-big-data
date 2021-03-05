6f796cf276e9536dd92d268acdb72ecc38534c47
Tyler Long <tyler4long@gmail.com>
2021-03-05 15:10:19 -0800

    Date picker

--- src/web/main.tsx
+++ src/web/main.tsx
@@ -1,23 +1,56 @@
 import React from 'react';
+import {DatePicker} from 'antd';
 
 import history from '../../data/history.json';
 import {highlight} from '../highlight';
 import symbols from '../symbols';
 
-const highlighted = highlight(history, {
-  symbols,
-  startDate: '20210219',
-  endDate: '20210305',
-  take: 100,
-  minMoneyAmount: 50000000000, // 50 billion
-});
+class App extends React.Component<{}, any> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      startDate: '',
+      endDate: '',
+      highlighted: [],
+    };
+  }
 
-class App extends React.Component {
+  display() {
+    if (this.state.startDate === '' || this.state.endDate === '') {
+      return;
+    }
+    this.setState({
+      highlighted: highlight(history, {
+        symbols,
+        startDate: this.state.startDate.replace(/-/g, ''),
+        endDate: this.state.endDate.replace(/-/g, ''),
+        take: 100,
+        minMoneyAmount: 50000000000, // 50 billion
+      }),
+    });
+  }
+
   render() {
     return (
-      <pre>
-        <code>{JSON.stringify(highlighted, null, 2)}</code>
-      </pre>
+      <>
+        <DatePicker
+          onChange={(date, dateString) => {
+            this.setState({startDate: dateString}, () => {
+              this.display();
+            });
+          }}
+        />
+        <DatePicker
+          onChange={(date, dateString) => {
+            this.setState({endDate: dateString}, () => {
+              this.display();
+            });
+          }}
+        />
+        <pre>
+          <code>{JSON.stringify(this.state.highlighted, null, 2)}</code>
+        </pre>
+      </>
     );
   }
 }
